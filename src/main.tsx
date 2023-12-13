06e94d84785353bb571aad8cfc692d50565a5fe6
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2023-12-13 12:52:52 +0000

    added routes and pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/Login'
+import Policy from './pages/Policy'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+  {
+    path: 'policy',
+    element: <Policy />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div>hello world</div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
