c0e11701a0a8b562764f6fd08af9191248d48782
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2023-12-14 10:55:45 +0000

    testing axios wip

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react'
-import { describe, it, expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import { describe, it, expect, vi } from 'vitest'
 import Login from './index.tsx'
+import axios from 'axios'
+
+vi.mock('axios')
 
 describe('Login', () => {
   it('renders a header with the correct text', () => {
@@ -19,6 +23,13 @@ describe('Login', () => {
 
   it('renders a submit button', () => {
     render(<Login />)
-    expect(screen.getByRole('button')).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /log in/i })).toBeInTheDocument()
+  })
+
+  it('call axios on submit', () => {
+    render(<Login />)
+    const user = userEvent.setup()
+    user.click(screen.getByRole('button', { name: /log in/i }))
+    expect(axios.post).toHaveBeenCalled()
   })
 })
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,14 @@
+import axios from 'axios'
+
 function Login() {
+  const handleClick = () => axios.post('')
+
   return (
     <>
       <div>Log in.</div>
       <input placeholder="Your Email Address"></input>
       <input placeholder="Your Password"></input>
-      <button>Log in</button>
+      <button onClick={handleClick}>Log in</button>
     </>
   )
 }
