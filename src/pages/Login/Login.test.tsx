351b1be6e76d04ab800b433421b5410cdc24ca2b
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2023-12-14 09:22:17 +0000

    added email and password input fields

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -5,6 +5,15 @@ import Login from './index.tsx'
 describe('Login', () => {
   it('renders a header with the correct text', () => {
     render(<Login />)
-    expect(screen.getByText(/Log in./i)).toBeInTheDocument()
+    expect(screen.getByText(/log in./i)).toBeInTheDocument()
+  })
+
+  it('renders an email and password input', () => {
+    render(<Login />)
+
+    expect(
+      screen.getByPlaceholderText(/your Email Address/i)
+    ).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/your Password/i)).toBeInTheDocument()
   })
 })
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,11 @@
 function Login() {
-  return <div>Log in.</div>
+  return (
+    <>
+      <div>Log in.</div>
+      <input placeholder="Your Email Address"></input>
+      <input placeholder="Your Password"></input>
+    </>
+  )
 }
 
 export default Login
