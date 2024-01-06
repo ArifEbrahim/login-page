10402d0ab7ba78319936fb50a7ca2dc6438e5e77
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2024-01-06 00:45:28 +0000

    added main styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/Login'
 import Policy from './pages/Policy'
+import './main.css'
 
 const router = createBrowserRouter([
   {
