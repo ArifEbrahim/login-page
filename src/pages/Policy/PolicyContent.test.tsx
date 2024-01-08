481e43acb67129b61ec6a00b0ee2f42929caf383
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2024-01-08 22:30:43 +0000

    policy content now displayed

--- src/pages/Policy/PolicyContent.test.tsx
+++ src/pages/Policy/PolicyContent.test.tsx
@@ -4,11 +4,29 @@ import { render, screen } from '@testing-library/react'
 
 describe('PolicyContent', () => {
   it('displays headers for different policy sections', () => {
-    render(<PolicyContent data={{}} />)
+    render(<PolicyContent />)
     expect(screen.getByText(/policy reference/i)).toBeInTheDocument()
     expect(screen.getByText(/cover type/i)).toBeInTheDocument()
     expect(screen.getByText(/car/i)).toBeInTheDocument()
-    expect(screen.getByText(/policy holder/i)).toBeInTheDocument()
+    expect(screen.getByText(/name/i)).toBeInTheDocument()
     expect(screen.getByText(/address/i)).toBeInTheDocument()
+  })
+
+  it('displays policy data', () => {
+    const mockData = {
+      policyRef: 'apple orange pear',
+      coverType: 'Comprehensive',
+      car: 'Tesla S Black - WO123XX',
+      name: 'Dave Jones',
+      address: 'Flat 1, 11 The Street, Little Hampton, W53RT'
+    }
+    render(<PolicyContent {...mockData} />)
+    expect(screen.getByText(/apple orange pear/i)).toBeInTheDocument()
+    expect(screen.getByText(/comprehensive/i)).toBeInTheDocument()
+    expect(screen.getByText(/tesla s black - WO123XX/i)).toBeInTheDocument()
+    expect(screen.getByText(/dave jones/i)).toBeInTheDocument()
+    expect(
+      screen.getByText(/flat 1, 11 the street, little hampton, W53RT/i)
+    ).toBeInTheDocument()
   })
 })
--- src/pages/Policy/PolicyContent.tsx
+++ src/pages/Policy/PolicyContent.tsx
@@ -1,11 +1,32 @@
-function PolicyContent({ data }) {
+interface FormattedPolicyData {
+  policyRef: string
+  coverType: string
+  car: string
+  name: string
+  address: string
+}
+
+type PolicyContentProps = FormattedPolicyData | Record<string, never>
+
+function PolicyContent({
+  policyRef,
+  coverType,
+  car,
+  name,
+  address
+}: PolicyContentProps) {
   return (
     <>
       <div>Policy reference</div>
+      <div>{policyRef}</div>
       <div>Cover type</div>
+      <div>{coverType}</div>
       <div>Car</div>
-      <div>Policy holder</div>
+      <div>{car}</div>
+      <div>Name</div>
+      <div>{name}</div>
       <div>Address</div>
+      <div>{address}</div>
     </>
   )
 }
