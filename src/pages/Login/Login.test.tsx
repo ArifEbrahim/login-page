68cba52cfc2dcbed7d18a2079caf43b41c3b20a6
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2024-02-08 05:31:36 +0000

    added error page for react router

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -106,22 +106,4 @@ describe('Login', () => {
     expect(localStorage.setItem).not.toHaveBeenCalled()
     expect(mockUseNavigate).not.toHaveBeenCalledWith('/policy')
   })
-
-  //   it('does not allow submission with invalid input', async () => {
-  //     const user = userEvent.setup()
-  //     render(<Login />)
-
-  //     const { emailInput, passwordInput, submitBtn } = getElements()
-
-  //     await user.click(submitBtn)
-  //     expect(axios.post).not.toHaveBeenCalled()
-
-  //     await user.type(emailInput, 'bob')
-  //     await user.click(submitBtn)
-  //     expect(axios.post).not.toHaveBeenCalled()
-
-  //     await user.type(passwordInput, 'abc123')
-  //     await user.click(submitBtn)
-  //     expect(axios.post).not.toHaveBeenCalled()
-  //   })
 })
